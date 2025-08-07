9e521fd03dfc97493fe6dcce3992a1b2559ce326
Vanshika <vanshikachhabra1407@gmail.com>
2025-08-07 14:12:54 +0530

    lec14

---
+++ lec-14/public/index.js
@@ -0,0 +1,36 @@
+
+let userContainer = document.querySelector('.user-container');
+
+
+function getUsers(URL){
+//send req to this URL to get users data
+fetch(URL)
+.then((res)=>{
+    console.log(res)
+    return res.json()
+})
+.then((data)=>{
+    console.log(data)
+    data.forEach((user) => {
+        displayUser(user);
+    });
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+}
+getUsers('/users')
+function displayUser(user){
+//user
+let li = document.createElement('li');
+li.innerHTML =`   <div class="user-info">
+            <h1>${user.name}</h1>
+            <p>${user.username}</p>
+        </div>
+        <div class="user-btn">
+            <button class="delete-btn">Delete</button>
+            <button class="edit-btn">Edit</button>
+        </div>`
+        userContainer.appendChild(li);
+}
---
+++ lec-14/users.js
@@ -0,0 +1,26 @@
+[
+    {
+        "id": 1,
+        "name": "John Doe",
+        "email": "john.doe@example.com",
+        "age": 28
+    },
+    {
+        "id": 2,
+        "name": "Jane Smith",
+        "email": "jane.smith@example.com",
+        "age": 24
+    },
+    {
+        "id": 3,
+        "name": "Mike Johnson",
+        "email": "mike.johnson@example.com",
+        "age": 32
+    },
+    {
+        "id": 4,
+        "name": "Sarah Wilson",
+        "email": "sarah.wilson@example.com",
+        "age": 27
+    }
+]
