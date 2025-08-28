17fc58a0e1cf2a917162805bb4451ff0f2529534
Vanshika <vanshikachhabra1407@gmail.com>
2025-08-28 14:37:47 +0530

    lec-17

--- lec-17/index.js
+++ lec-17/index.js
@@ -1,51 +1,97 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
-const Blog = require('./model/blog');
+const app = express();  
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//create
-app.post('/blogs', async(req, res) => {
-    let title = req.body.title;
-    let body = req.body.body;   
-    let blog={
-        title: title,
-        body: body,
-        date: Date.now()
 
+const Blog = require('./models/blog');
+const User = require('./models/user');
+
+// create
+app.post("/blogs", async(req,res)=>{
+    let title = req.body.title;
+    let body = req.body.body;
+    let blog = {
+        title : title,
+        body : body,
+        date : Date.now()
     }
-    let newBlog=new Blog(blog)
+    let newBlog = new Blog(blog) 
     await newBlog.save()
     res.json({
-        success: true,
-        message: "Blog created successfully",
-        data: newBlog
-    });
+        success : true,
+        message : "blog added successfully",
+        data : newBlog
+    })
 })
 
-//read
-//read all data
-//read single data
-app.get('/blogs', async(req, res) => {
+// read
+// read all data
+app.get("/blogs",async (req,res)=>{
     let allBlogs = await Blog.find();
     res.json({
-        success: true,
-        message: "all data fetched successfully",
-        data: allBlogs
-    });
-});
-app.get('/blogs/:id', async(req, res) => {
+        success : true,
+        message : "all data fetched successfully",
+        data : allBlogs
+    })
+})
+// read single data
+app.get("/blogs/:id",async (req,res)=>{
     let id = req.params.id;
-    let blog = await Blog.findById(id);
+    let blog = await Blog.findById(id); 
     res.json({
-        success: true,  
-        message: "single data fetched successfully",
-        data: blog  
-    });
-});
+        success : true,
+        message : "blog fetched successfully",
+        data : blog
+    })
+})
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+
+
+// create user
+app.post("/user", async (req,res)=>{
+    let name = req.body.name;
+    let email = req.body.email;
+    let password = req.body.password;
+    let user = {
+        name : name,
+        email : email,
+        password : password
+    }
+    let newUser = new User(user)
+    await newUser.save()
+    res.json({
+        success : true,
+        message : "user added successfully",
+        data : newUser
+    })
+})
+
+// read
+// read all users
+app.get("/user", async (req,res)=>{
+    let allUsers = await User.find();
+    res.json({
+        success : true,
+        message : "all users fetched successfully",
+        data : allUsers
+    })
+})
+
+// read single user
+app.get("/user/:id", async(req,res)=>{
+    let id = req.params.id;
+    let user = await User.findById(id);
+    res.json({
+        success : true,
+        message : "user fetched successfully",
+        data : user
+    })
+})
+
+mongoose.connect('mongodb://127.0.0.1:27017/G27DBs')
   .then(() => console.log('Connected!'));
-app.listen(3400, () => {
-  console.log('Server is running on port 3400');
+
+app.listen(3600, () => {
+    console.log(`Server is running on http://localhost:3600`);
 });
